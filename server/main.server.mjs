5ee6ddbb36292b8e125e0397dfaa3ff6c3c02f90
Custom Zombicide <marvelzombicide2024@gmail.com>
2024-03-29 17:10:35 -0500

    Add all heroes and zombies

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WNKHVIQN.mjs";import"./chunk-KFFQTAJD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HF3B5O5D.mjs";import"./chunk-KFFQTAJD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
